997ad2474811a9326e2b63c085e9f2f47cbba01c
Robbie <saucy.cooking@gmail.com>
2019-03-15 10:47:02 -0600

    fixed chat bug

--- src/About/ChatContainer/ChatContainer.js
+++ src/About/ChatContainer/ChatContainer.js
@@ -60,6 +60,7 @@ class ChatContainer extends Component {
         };
         this.setState({
             message:{
+                ...this.state.message,
                 text: ""
             }
         });
